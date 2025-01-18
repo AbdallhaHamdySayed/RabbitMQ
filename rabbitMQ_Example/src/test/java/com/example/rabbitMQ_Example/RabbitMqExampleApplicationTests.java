47a0b58d74a12e55a7c5bb6e106b59301c8d2bbd
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-01-18 06:01:54 +0200

    Sample Message

---
+++ rabbitMQ_Example/src/test/java/com/example/rabbitMQ_Example/RabbitMqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitMQ_Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
