626d09ceca216ffd77ff05073e46748c13e78a13
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-01-18 07:07:11 +0200

    Spring Boot RabbitMQ Multiple Queues Example

---
+++ Spring-Boot-RabbitMQ-Multiple-Queues-Example/src/main/java/com/example/Spring/Boot/RabbitMQ/Multiple/Queues/Example/dto/User.java
@@ -0,0 +1,10 @@
+package com.example.Spring.Boot.RabbitMQ.Multiple.Queues.Example.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
